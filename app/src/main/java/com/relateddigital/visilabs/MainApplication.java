4b2b5817c4a90921884c2f3207510bb8c22d4881
emre.eken <emre.eken@euromsg.com>
2021-01-18 11:05:51 +0300

    Get Visilabs.CreateAPI() parameters from Manifest All parameters and appAlias are now being gotten from Manifest Replaces deprecated Firabase class/method with the new one.

--- app/src/main/java/com/relateddigital/visilabs/MainApplication.java
+++ app/src/main/java/com/relateddigital/visilabs/MainApplication.java
@@ -1,11 +1,14 @@
 package com.relateddigital.visilabs;
 
 import android.app.Application;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.visilabs.Visilabs;
 import com.visilabs.util.VisilabsConstant;
 import java.util.HashMap;
@@ -13,47 +16,68 @@ import euromsg.com.euromobileandroid.EuroMobileManager;
 
 public class MainApplication extends Application {
 
-    public static String ORGANIZATION_ID = "676D325830564761676D453D";
-    public static String SITE_ID = "356467332F6533766975593D";
-    public static String DATASOURCE = "visistore";
-    public static String APPALIAS = "visilabs-android-test";
-
-
-    EuroMobileManager euroMobileManager;
+    private String appAlias;
+    private EuroMobileManager euroMobileManager;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Visilabs.CreateAPI(ORGANIZATION_ID, SITE_ID, "http://lgr.visilabs.net",
-                DATASOURCE, "http://rt.visilabs.net", "Android", getApplicationContext(),  "http://s.visilabs.net/json"
-                , "http://s.visilabs.net/actjson", 30000, "http://s.visilabs.net/geojson", true);
+
+        ApplicationInfo appInfo = null;
+        Bundle bundle = null;
+
+        try{
+            appInfo = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+        }
+
+        if(appInfo!=null){
+            bundle = appInfo.metaData;
+        }
+
+        if(bundle!=null){
+            appAlias = bundle.getString("AppAlias", "");
+            Visilabs.CreateAPI(
+                    bundle.getString("VisilabsOrganizationID", ""),
+                    bundle.getString("VisilabsSiteID", ""),
+                    bundle.getString("VisilabsSegmentURL", ""),
+                    bundle.getString("VisilabsDataSource", ""),
+                    bundle.getString("VisilabsRealTimeURL", ""),
+                    bundle.getString("VisilabsChannel", ""),
+                    getApplicationContext(),
+                    bundle.getString("VisilabsTargetURL", ""),
+                    bundle.getString("VisilabsActionURL", ""),
+                    bundle.getInt("VisilabsRequestTimeoutInSeconds", 30),
+                    bundle.getString("VisilabsGeofenceURL", ""),
+                    bundle.getBoolean("VisilabsGeofenceEnabled", false)
+            );
+        }
+
         VisilabsConstant.DEBUG = true;
 
 
-        euroMobileManager = EuroMobileManager.init(APPALIAS, null, getApplicationContext());
+        euroMobileManager = EuroMobileManager.init(appAlias, null, getApplicationContext());
         euroMobileManager.registerToFCM(getBaseContext());
         setExistingFirebaseTokenToEuroMessage();
     }
 
     private void setExistingFirebaseTokenToEuroMessage() {
 
-        FirebaseInstanceId.getInstance().getInstanceId()
-                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
                     @Override
-                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
-
+                    public void onComplete(@NonNull Task<String> task) {
                         if (!task.isSuccessful()) {
                             return;
                         }
-
-                        String token = task.getResult().getToken();
+                        String token = task.getResult();
                         HashMap<String, String> parameters= new HashMap<String, String>();
                         parameters.put("OM.sys.TokenID", token);
-                        parameters.put("OM.sys.AppID", APPALIAS);
+                        parameters.put("OM.sys.AppID", appAlias);
                         Visilabs.CallAPI().customEvent("Register Token", parameters);
                         euroMobileManager.subscribe(token, getApplicationContext());
                     }
                 });
     }
-
 }
